54f0a770ae5e858ce774f2c5e93741649a9681cd
t-daisuke <t.daisuke.64@gmail.com>
2024-09-25 22:20:39 +0900

    コメントを返した

--- main.go
+++ main.go
@@ -11,27 +11,34 @@ import (
 
 func main() {
 	// .envファイルを読み込む
+	fmt.Println("Loading .env file...")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
+	fmt.Println(".env file loaded successfully.")
 
 	// .envファイルからSLACK_BOT_TOKENを取得
 	token := os.Getenv("SLACK_BOT_TOKEN")
 	if token == "" {
 		log.Fatalf("SLACK_BOT_TOKEN is not set in .env file")
 	}
+	fmt.Println("SLACK_BOT_TOKEN retrieved successfully.")
 
+	// Slack APIクライアントを作成
 	api := slack.New(token)
+	fmt.Println("Slack API client initialized.")
 
 	// WebSocketを使ってリアルタイムでイベントを処理
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
+	fmt.Println("WebSocket connection started.")
 
 	// イベントを処理するループ
 	for msg := range rtm.IncomingEvents {
-		switch ev := msg.Data.(type) {
+		fmt.Println("Event received: ", msg)
 
+		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
 			// メッセージが投稿された場合の処理
 			fmt.Printf("Message: %v\n", ev)
@@ -43,6 +50,7 @@ func main() {
 			}
 		default:
 			// 他のイベントは無視
+			fmt.Println("Unhandled event type")
 		}
 	}
 }
